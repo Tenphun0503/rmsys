7b8cc559e8567dbee8c74981b104b5e50a98d80e
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-04-15 22:38:21 -0400

    Add log output

--- src/main/java/com/tenphun/rmsys/RmsysApplication.java
+++ src/main/java/com/tenphun/rmsys/RmsysApplication.java
@@ -1,13 +1,14 @@
 package com.tenphun.rmsys;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RmsysApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RmsysApplication.class, args);
+        log.info("[INFO] System started successfully...");
     }
-
 }
